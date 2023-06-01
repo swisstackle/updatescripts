5661a0e0f57cb6a4440577230e1cdffd2ee67155
Alain Schaerer <alainsch@buffalo.edu>
2023-06-01 18:10:46 -0400

    Create notepad.bat

---
+++ notepad.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Download the latest version of Notepad++
+powershell -Command "Invoke-WebRequest -OutFile notepadppInstaller.exe -Uri https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.3/npp.8.5.3.Installer.x64.exe"
+
+REM Uninstall Notepad++
+start /wait "" "C:\Program Files (x86)\Notepad++\uninstall.exe" /S
+
+REM Pause the script for a while to make sure the uninstaller completes
+timeout /t 10
+
+REM Install Notepad++
+start /wait "" notepadppInstaller.exe /S
+
+echo Installation Complete!
