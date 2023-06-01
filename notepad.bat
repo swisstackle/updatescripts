9b0109866b4fafc2a051a31efa61be654b266901
Alain Schaerer <alainsch@buffalo.edu>
2023-06-01 18:52:27 -0400

    Update notepad.bat

--- notepad.bat
+++ notepad.bat
@@ -3,7 +3,9 @@ REM Download the latest version of Notepad++
 powershell -Command "Invoke-WebRequest -OutFile notepadppInstaller.exe -Uri https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.3/npp.8.5.3.Installer.x64.exe"
 
 REM Uninstall Notepad++
+if exist C:\Program Files (x86)\Notepad++\uninstall.exe (
 start /wait "" "C:\Program Files (x86)\Notepad++\uninstall.exe" /S
+)
 
 REM Pause the script for a while to make sure the uninstaller completes
 timeout /t 10
