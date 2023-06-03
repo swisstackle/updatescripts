9700033acce2b6ceeeb79a09ca906d1b71b16857
Alain Schaerer <alainsch@buffalo.edu>
2023-06-03 13:54:09 -0400

    Update all.bat

--- all.bat
+++ all.bat
@@ -1,21 +1,12 @@
 echo @off
 setlocal
-if exist "C:\Program Files (x86)\Zotero\uninstall\helper.exe" (
 "C:\Program Files (x86)\Zotero\uninstall\helper.exe" /S
-)
+"C:\Program Files\Zotero\uninstall\helper.exe" /S
 "%appdata%\Zoom\bin\Zoom_launcher.exe"
-if exist "C:\Program Files\VideoLAN\VLC\uninstall.exe" (
 "C:\Program Files\VideoLAN\VLC\uninstall.exe" /S
-)
+"C:\Program Files (x86)\VideoLAN\VLC\uninstall.exe" /S
 "C:\Program Files\Notepad++\uninstall.exe" /S
 "C:\Program Files (x86)\Notepad++\uninstall.exe" /S
-if exist "C:\Program Files\Mozilla Firefox\uninstall\helper.exe" (
 "C:\Program Files\Mozilla Firefox\uninstall\helper.exe" /S
-)
-if exist C:\Program Files\Google\Chrome\Application\*\Installer\setup.exe (
 "C:\Program Files\Google\Chrome\Application\*\Installer\setup.exe" --uninstall --multi-install --chrome --system-level --force-uninstall
-)
-if exist C:\Program Files (x86)\Google\Chrome\Application\*\Installer\setup.exe 
-(
 "C:\Program Files (x86)\Google\Chrome\Application\*\Installer\setup.exe" --uninstall --multi-install --chrome --system-level --force-uninstall
-)
